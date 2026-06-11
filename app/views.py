c63d2698d2df91c6cb9316587acc9527de476a5d
i-suzuki <i-suzuki@example.com>
2026-06-11 10:55:32 +0900

    管理者login機能: login画面の作成、toppage: hello world追加

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def index(request):
+    return render(request, "app/test.html")
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("index")
+    else:
+        form = UserCreationForm()
+    return render(request, "app/signup.html", {"form": form})
